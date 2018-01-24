363c00fdb4461bda397ddb99028db8aaee8461a1
Abai <abai2010@bk.ru>
2018-01-24 14:11:48 +0600

    finished

---
+++ hw21_WPF_Elements2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace hw21_WPF_Elements2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ hw21_WPF_Elements2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hw21_WPF_Elements2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string buffer;
+        private int position;
+        public MainWindow()
+        {
+            InitializeComponent();
+            buffer = "";
+        }
+
+        private void CopyButtonClick(object sender, RoutedEventArgs e)
+        {
+            buffer = textBox.SelectedText;
+            position = textBox.CaretIndex;
+        }
+
+        private void CutButtonClick(object sender, RoutedEventArgs e)
+        {
+            buffer = textBox.SelectedText;
+            if (buffer.Length != 0)
+            {
+                textBox.Text = textBox.Text.Replace(buffer, "");    
+            }
+        }
+
+        private void PasteButtonClick(object sender, RoutedEventArgs e)
+        {
+            position = textBox.CaretIndex;
+            textBox.Text = textBox.Text.Insert(position, buffer);
+        }
+    }
+}
